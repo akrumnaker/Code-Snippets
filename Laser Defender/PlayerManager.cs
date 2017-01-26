299fb13a04567299298cefd0e9fab19cad381e9c
Aaron Krumnaker <akrumnaker@gmail.com>
2017-01-25 20:07:09 -0500

    Add files via upload
    This file contains functions used in keeping track of the number of lives during the game as well as the respawning of the player when they have died and have lives remaining.

---
+++ Laser Defender/PlayerManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+    public static PlayerManager manager;
+    public int lives = 3;
+    public GameObject player;
+    public Text livesCounter;
+
+    void Start()
+    {
+        if(manager != this)
+        {
+            manager = this;
+        }
+    }
+
+    public void AddLife()
+    {
+        // Add a life and adjust livesCounter text
+        lives++;
+        livesCounter.text = "X" + lives;
+    }
+
+    public void RespawnPlayer()
+    {
+        lives--;
+        livesCounter.text = "X" + lives;
+        // Load the Win Screen if the player has no lives left
+        if (lives <= 0)
+        {
+            LevelManager lvlMngr = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+            lvlMngr.LoadLevel("Win Screen");
+        }else
+        {
+            //Instantiate new instance of player object
+            Instantiate(player, transform.position, Quaternion.identity);
+        }
+    }
+}
